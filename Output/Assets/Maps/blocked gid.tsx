dbaf53fcae0409de8b4e565844240bab23c5d798
pcelaya <04polcelaya@gmail.com>
2024-04-14 18:17:58 +0200

    Level 3 - Mapa 3

---
+++ Output/Assets/Maps/blocked gid.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="blocked gid" tilewidth="128" tileheight="64" tilecount="2" columns="1" objectalignment="bottomleft">
+ <tileoffset x="0" y="32"/>
+ <image source="128.png" width="128" height="128"/>
+</tileset>
